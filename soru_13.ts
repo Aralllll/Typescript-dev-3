c4113cd551ccde8aed0a26073fbe1886937acd2d
Aralllll <aralmain410@gmail.com>
2025-11-06 23:40:05 +0300

    Create soru_13.ts

---
+++ soru_13.ts
@@ -0,0 +1,19 @@
+type CreateGetters<T> = {
+  [K in keyof T as `get${Capitalize<string & K>}`]: () => T[K];
+}
+
+type UserProps = { id: number; name: string; };
+type UserGetters = CreateGetters<UserProps>;
+
+const userGetters: UserGetters = {
+  getId: () => 1,
+  getName: () => "Ali"
+};
+
+// @ts-expect-error - Dönüş tipi 'string' olmalı, 'number' değil.
+const errorGetter: UserGetters = {
+  getId: () => 1,
+  getName: () => 123
+};
+
+console.log("Soru 13 Başarılı!", userGetters.getName());
